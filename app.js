ac109bba9e24e6d049b2261c6fd95ee02cce8560
jrang188 <jrang@sfu.ca>
2022-04-02 15:58:38 -0700

    added joi

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const {campgroundSchema} = require('./schemas.js')
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -22,10 +24,20 @@ const app = express();
 // View Engine Setup
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 app.engine("ejs", ejsMate); // config express to use ejs-mate as a custom template engine
+
 app.use(express.urlencoded({ extended: true })); // initialize middleware that parses urlencoded bodies
 app.use(methodOverride("_method")); // initialize method override to allow us to use the _method parameter in our form
+
+const validateCampground = (req, res, next) => {
+  const {error} = campgroundSchema.validate(req.body);
+  if(error){
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+}
 
 //root route
 app.get("/", (req, res) => {
@@ -44,7 +56,8 @@ app.get("/campgrounds/new", catchAsync(async (req, res) => {
 }));
 
 // CREATE route - add new campground to DB
-app.post("/campgrounds", catchAsync(async(req, res, next) => {
+app.post("/campgrounds", validateCampground, catchAsync(async(req, res, next) => {
+  // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground.id}`);
@@ -63,7 +76,7 @@ app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
 }));
 
 // UPDATE route - update campground in DB
-app.put("/campgrounds/:id", catchAsync(async (req, res) => {
+app.put("/campgrounds/:id", validateCampground, catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
@@ -78,8 +91,14 @@ app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
   res.redirect("/campgrounds");
 }));
 
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
+})
+
 app.use((err, req, res, next) => {
-  res.send("OH NO!!");
+  const {statusCode = 500, } = err;
+  if(!err.message) err.message = 'OH NO !! Something went wrong!'
+  res.status(statusCode).render('error', {err});
 });
 
 // Port Setup
