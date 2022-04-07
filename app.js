6645148231ef902a430e868591708b24a90cbb9f
jrang188 <jrang@sfu.ca>
2022-04-06 23:11:50 -0700

    fixed delete route

--- app.js
+++ app.js
@@ -141,6 +141,7 @@ app.post(
 app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
   const {id, reviewId} = req.params;
   await Campground.findByIdAndUpdate(id, {$pull : {reviews: reviewId}});
+  await Review.findByIdAndDelete(reviewId);
   res.redirect(`/campgrounds/${id}`);
 }))
 
