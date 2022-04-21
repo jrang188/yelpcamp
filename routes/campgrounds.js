4fb650b6467986b9a5b93908f1252ece9c55e88b
jrang188 <jrang@sfu.ca>
2022-04-21 00:28:44 -0700

    setup cloudinary env and multer. TODO: fix issue regarding uploads of multiple images

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router
   .route('/')
   .get(catchAsync(campgrounds.index))
-  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+  // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+  .post(upload.array('image'), (req, res) => {
+    console.log(req.body, req.file);
+    res.send('it worked?');
+  });
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
